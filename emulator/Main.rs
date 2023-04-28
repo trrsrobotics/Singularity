2ff29ac5a199862107554fbca7f35459e9bcbc9d
0xC0ba1t <73546596+0xC0ba1t@users.noreply.github.com>
2023-04-28 22:48:50 +0000

    Update Main.rs
    Fix some typos

--- emulator/Main.rs
+++ emulator/Main.rs
@@ -7,12 +7,12 @@ use std::io;
 fn main() {
     let mut file_path = String::new();
     let stdin = io::stdin;
-    println!("Enter file path to instructions:")
+    println!("Enter file path to instructions:");
     stdin.read_line(&mut file_path)?;
     println!("Reading instructions from file: {} ", file_path)
     let instr = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
-    println!("Read instructions from file correctly!")
+    println!("Read instructions from file correctly!");
     // todo => write assembler
 
     Ok(())
