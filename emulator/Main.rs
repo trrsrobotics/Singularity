59553ba2e072c1ed5d7fdcfdb0b339f4ab4650da
0xC0ba1t <73546596+0xC0ba1t@users.noreply.github.com>
2023-04-28 22:53:28 +0000

    Update Main.rs
    Fix error, method not found

--- emulator/Main.rs
+++ emulator/Main.rs
@@ -8,7 +8,7 @@ fn main() -> io::Result<()> {
     let mut file_path = String::new();
     let stdin = io::stdin;
     println!("Enter file path to instructions:");
-    stdin.read_line(&mut file_path)?;
+    stdin().read_line(&mut file_path)?;
     println!("Reading instructions from file: {} ", file_path);
     let instr = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
