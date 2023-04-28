c8e28cf818f7bccec961dd0a3db7386176fee866
0xC0ba1t <73546596+0xC0ba1t@users.noreply.github.com>
2023-04-28 22:45:32 +0000

    Create Main.rs
    Start work on emulator

---
+++ emulator/Main.rs
@@ -0,0 +1,19 @@
+// Copyright © The Roman Ridge School 2023-Present
+
+use std::env;
+use std::fs;
+use std::io;
+
+fn main() {
+    let mut file_path = String::new();
+    let stdin = io::stdin;
+    println!("Enter file path to instructions:")
+    stdin.read_line(&mut file_path)?;
+    println!("Reading instructions from file: {} ", file_path)
+    let instr = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    println!("Read instructions from file correctly!")
+    // todo => write assembler
+
+    Ok(())
+}
